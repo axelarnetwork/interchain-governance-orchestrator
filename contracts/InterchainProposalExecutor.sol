83dd212138836d4c48e8f6360f97ce151699424f
npty <nptytn2@gmail.com>
2023-05-03 12:13:00 +0700

    chore: update contracts

--- contracts/InterchainProposalExecutor.sol
+++ contracts/InterchainProposalExecutor.sol
@@ -7,9 +7,41 @@ import "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecu
 contract InterchainProposalExecutor is AxelarExecutable {
     constructor(address _gateway) AxelarExecutable(_gateway) {}
 
+    /**
+     * @dev Execute the proposal
+     * @param payload The payload. The payload is ABI encoded array of targets, values, signatures and data.
+     * Where:
+     * - `targets` are the contracts to call
+     * - `values` are the amounts of native tokens to send
+     * - `signatures` are the function signatures to call
+     * - `data` is the encoded function arguments.
+     */
     function _execute(
-        string calldata sourceChain,
-        string calldata sourceAddress,
+        string calldata,
+        string calldata,
         bytes calldata payload
-    ) internal override {}
+    ) internal override {
+        // Decode the payload
+        (
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory signatures,
+            bytes[] memory data
+        ) = abi.decode(payload, (address[], uint256[], bytes[], bytes[]));
+
+        // Iterate over all targets and call them with the given data
+        for (uint256 i = 0; i < targets.length; i++) {
+            // Construct the call data
+            bytes memory callData = abi.encodePacked(
+                bytes4(keccak256(bytes(signatures[i]))),
+                data[i]
+            );
+
+            // Call the target
+            (bool success, ) = targets[i].call{value: values[i]}(callData);
+
+            // Revert if the call failed
+            require(success, "InterchainProposalExecutor: call failed");
+        }
+    }
 }
--- contracts/InterchainProposalSender.sol
+++ contracts/InterchainProposalSender.sol
@@ -15,11 +15,33 @@ contract InterchainProposalSender is Ownable {
         gasService = IAxelarGasService(_gasService);
     }
 
+    /**
+     * @dev Propose an interchain transaction
+     * @param destinationChain The destination chain
+     * @param destinationContract The destination contract
+     * @param payload The payload. The payload is ABI encoded array of targets, values, signatures and data.
+     */
     function interchainPropose(
         string memory destinationChain,
         string memory destinationContract,
         bytes memory payload
     ) public payable onlyOwner {
+        // check if payload is valid
+        (
+            address[] memory targets,
+            uint256[] memory values,
+            bytes[] memory signatures,
+            bytes[] memory data
+        ) = abi.decode(payload, (address[], uint256[], bytes[], bytes[]));
+
+        require(targets.length > 0, "InterchainProposalSender: no targets");
+        require(
+            targets.length == values.length &&
+                targets.length == signatures.length &&
+                targets.length == data.length,
+            "InterchainProposalSender: invalid payload"
+        );
+
         if (msg.value > 0) {
             gasService.payNativeGasForContractCall{value: msg.value}(
                 address(this),
