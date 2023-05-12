06c5c2ec668edaddb7a2f7107bd524498446c802
npty <nptytn2@gmail.com>
2023-05-12 14:31:06 +0700

    chore: add deploy functions

--- test/utils/deploy.ts
+++ test/utils/deploy.ts
@@ -1,6 +1,7 @@
 import { Wallet } from "ethers";
 import { ethers } from "hardhat";
 import { getChains } from "./chains";
+import config from "../../hardhat.config";
 
 export async function deploy(
   wallet: Wallet,
@@ -37,4 +38,42 @@ export function deployDummyProposalExecutor(deployer: Wallet) {
 export function deployDummyState(deployer: Wallet) {
   const chains = getChains();
   return deploy(deployer, chains[1].rpc, "DummyState", []);
+}
+
+export function deployTimelock(deployer: Wallet) {
+  const chains = getChains();
+  return deploy(deployer, chains[0].rpc, "Timelock", [deployer.address, "3"]);
+}
+
+export function deployComp(deployer: Wallet) {
+  const chains = getChains();
+  return deploy(deployer, chains[0].rpc, "Comp", [deployer.address]);
+}
+
+export async function deployGovernorBravo(
+  deployer: Wallet,
+  timelockAddress: string,
+  compAddress: string
+) {
+  const chains = getChains();
+  const votingPeriod = "5"; // 5 seconds
+  const votingDelay = "0"; // No delay
+  const proposalThreshold = ethers.utils.parseEther("1000").toString(); // 1000 COMP
+  const contract = await deploy(
+    deployer,
+    chains[0].rpc,
+    "GovernorBravoDelegate",
+    []
+  );
+  const args = [
+    timelockAddress,
+    compAddress,
+    votingPeriod,
+    votingDelay,
+    proposalThreshold,
+  ];
+
+  await contract.initialize(...args);
+
+  return contract;
 }
