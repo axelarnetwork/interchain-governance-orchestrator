bf95212bc0c376a66ce61a5d3daf2ecb37b029f0
npty <nptytn2@gmail.com>
2023-05-16 08:55:52 +0700

    chore: update interchain proposal for access control

--- contracts/InterchainProposalExecutor.sol
+++ contracts/InterchainProposalExecutor.sol
@@ -3,11 +3,28 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
+import "@axelar-network/axelar-gmp-sdk-solidity/contracts/utils/AddressString.sol";
 
-contract InterchainProposalExecutor is AxelarExecutable {
-    constructor(address _gateway) AxelarExecutable(_gateway) {}
+contract InterchainProposalExecutor is AxelarExecutable, Ownable {
+    address public sourceInterchainSender;
+    mapping(address => bool) public whitelistedProposalCallers;
+
+    constructor(
+        address _gateway,
+        address _sourceInterchainSender
+    ) AxelarExecutable(_gateway) {
+        sourceInterchainSender = _sourceInterchainSender;
+    }
 
     event ProposalExecuted(bytes32 indexed payloadHash);
+
+    modifier onlyWhitelistedCaller(address caller) {
+        require(
+            whitelistedProposalCallers[caller],
+            "InterchainProposalExecutor: caller is not whitelisted"
+        );
+        _;
+    }
 
     /**
      * @dev Execute the proposal
@@ -19,18 +36,47 @@ contract InterchainProposalExecutor is AxelarExecutable {
      * - `data` is the encoded function arguments.
      */
     function _execute(
-        string calldata,
+        string calldata sourceAddress,
         string calldata,
         bytes calldata payload
     ) internal override {
+        // Check that the source address is the same as the source interchain sender
+        require(
+            StringToAddress.toAddress(sourceAddress) == sourceInterchainSender,
+            "InterchainProposalExecutor: source address is not the same as the source interchain sender"
+        );
+
         // Decode the payload
         (
+            address interchainProposalCaller,
             address[] memory targets,
             uint256[] memory values,
             string[] memory signatures,
             bytes[] memory data
-        ) = abi.decode(payload, (address[], uint256[], string[], bytes[]));
+        ) = abi.decode(
+                payload,
+                (address, address[], uint256[], string[], bytes[])
+            );
 
+        // Execute the proposal
+        _executeProposal(
+            interchainProposalCaller,
+            targets,
+            values,
+            signatures,
+            data
+        );
+
+        emit ProposalExecuted(keccak256(payload));
+    }
+
+    function _executeProposal(
+        address proposalCaller,
+        address[] memory targets,
+        uint256[] memory values,
+        string[] memory signatures,
+        bytes[] memory data
+    ) internal onlyWhitelistedCaller(proposalCaller) {
         // Iterate over all targets and call them with the given data
         for (uint256 i = 0; i < targets.length; i++) {
             // Construct the call data
@@ -45,7 +91,18 @@ contract InterchainProposalExecutor is AxelarExecutable {
             // Revert if the call failed
             require(success, "InterchainProposalExecutor: call failed");
         }
+    }
 
-        emit ProposalExecuted(keccak256(payload));
+    function setWhitelistedProposalCaller(
+        address caller,
+        bool whitelisted
+    ) external onlyOwner {
+        whitelistedProposalCallers[caller] = whitelisted;
+    }
+
+    function setSourceInterchainSender(
+        address _sourceInterchainSender
+    ) external onlyOwner {
+        sourceInterchainSender = _sourceInterchainSender;
     }
 }
