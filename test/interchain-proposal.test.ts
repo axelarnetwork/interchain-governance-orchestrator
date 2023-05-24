0e2df13779ac02101f1a5ea7aba2860861ca45be
npty <nptytn2@gmail.com>
2023-05-24 11:30:02 +0700

    chore: reword test

--- test/interchain-proposal.test.ts
+++ test/interchain-proposal.test.ts
@@ -77,7 +77,7 @@ describe("Interchain Proposal", function () {
     await stop();
   });
 
-  it("should execute a proposal with to a single target contract", async function () {
+  it("should execute a proposal with a single destination target contract", async function () {
     // Encode the payload for the destination chain
     const payload = ethers.utils.defaultAbiCoder.encode(
       ["address[]", "uint256[]", "string[]", "bytes[]"],
@@ -132,7 +132,7 @@ describe("Interchain Proposal", function () {
     await expect(await dummyState.message()).to.equal("Hello World");
   });
 
-  it("should execute a proposal with to multiple target contracts", async function () {
+  it("should execute a proposal with multiple destination target contracts", async function () {
     const dummyState2 = await deployDummyState(deployer);
     const dummyState3 = await deployDummyState(deployer);
 
