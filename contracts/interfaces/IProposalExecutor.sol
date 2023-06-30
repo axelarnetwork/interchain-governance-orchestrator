f49d5d487c1068d18175b353f511eaf2d9e7440e
Milap Sheth <milap@axelar.network>
2023-06-30 05:25:44 -0400

    less restrictive pragmas

--- contracts/interfaces/IProposalExecutor.sol
+++ contracts/interfaces/IProposalExecutor.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.0;
 
 interface IProposalExecutor {
     // An event emitted when the proposal caller is whitelisted
--- contracts/test/DummyState.sol
+++ contracts/test/DummyState.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 contract DummyState {
     string public message;
