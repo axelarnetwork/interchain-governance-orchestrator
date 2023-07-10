bf5c0fc8ecf2b050ff7392f0266abd04788441f3
npty <nptytn2@gmail.com>
2023-07-10 18:30:14 +0700

    chore: update deployment salt

--- deploy/03-deploy-dummy-state.ts
+++ deploy/03-deploy-dummy-state.ts
@@ -1,15 +1,16 @@
 import { DeployFunction } from "hardhat-deploy/dist/types";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { deploy3 } from "../scripts/deploy";
-import { contracts } from "../constants";
 
 const contractName = "DummyState";
 const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const [deployer] = await hre.getUnnamedAccounts();
-  await deploy3(hre, contractName, deployer + "v1", []);
+  const artifact = await hre.artifacts.readArtifact(contractName);
+  const salt = [deployer, artifact.bytecode, "v1", "test"].join()
+  await deploy3(hre, contractName, salt, []);
 };
 
 deploy.tags = [contractName];
-deploy.dependencies = ["DummyState"];
+deploy.dependencies =["Deployer"];
 
 export default deploy;
