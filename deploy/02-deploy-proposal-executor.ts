168a781f18e2254ac2f9416593eafd1caca4aced
npty <nptytn2@gmail.com>
2023-07-10 18:40:13 +0700

    chore: update deploy salt

--- deploy/02-deploy-proposal-executor.ts
+++ deploy/02-deploy-proposal-executor.ts
@@ -8,7 +8,7 @@ const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const [deployer] = await hre.getUnnamedAccounts();
   const { gateway } = contracts[hre.network.name];
   const artifact = await hre.artifacts.readArtifact(contractName);
-  const salt = [deployer, artifact.bytecode, "v1", "test"].join()
+  const salt = [deployer, artifact.bytecode, "v1"].join()
   await deploy3(hre, contractName, salt, [gateway, deployer]);
 };
 
--- deploy/03-deploy-dummy-state.ts
+++ deploy/03-deploy-dummy-state.ts
@@ -6,7 +6,7 @@ const contractName = "DummyState";
 const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
   const [deployer] = await hre.getUnnamedAccounts();
   const artifact = await hre.artifacts.readArtifact(contractName);
-  const salt = [deployer, artifact.bytecode, "v1", "test"].join()
+  const salt = [deployer, artifact.bytecode, "v1"].join()
   await deploy3(hre, contractName, salt, []);
 };
 
