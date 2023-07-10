99fffdfe72b63faab72f2b8da95d5c0ce12c8c76
npty <nptytn2@gmail.com>
2023-07-10 18:30:46 +0700

    chore: add execute dummy state task

--- tasks/executeDummyState.ts
+++ tasks/executeDummyState.ts
@@ -5,22 +5,32 @@ import {
   InterchainProposalSender,
 } from "../typechain-types";
 import { AxelarQueryAPI, Environment } from "@axelar-network/axelarjs-sdk";
+import {chains as testnetChains } from "@axelar-network/axelar-contract-deployments/info/testnet.json";
+import {chains as mainnetChains} from "@axelar-network/axelar-contract-deployments/info/mainnet.json";
+import { getDeploymentAddress } from "./helpers/deployment";
 
 task(
   "executeDummyState",
   "Update message on DummyState contract from source chain"
 )
   .addPositionalParam("destinationChain")
-  .addPositionalParam("executorContractAddress")
-  .addPositionalParam("dummyContractAddress")
   .addPositionalParam("message")
+  .addOptionalPositionalParam("executorContractAddress")
+  .addOptionalPositionalParam("dummyContractAddress")
   .setAction(async (taskArgs, hre) => {
     const {
       destinationChain,
-      executorContractAddress,
-      dummyContractAddress,
       message,
+      executorContractAddress: _executorContractAddress,
+      dummyContractAddress: _dummyContractAddress,
     } = taskArgs;
+
+    const executorContractAddress = _executorContractAddress || getDeploymentAddress(hre, "InterchainProposalExecutor", destinationChain);
+    const dummyContractAddress = _dummyContractAddress || getDeploymentAddress(hre, "DummyState", destinationChain);
+
+    if(!executorContractAddress || !dummyContractAddress) {
+      throw new Error("executorContractAddress or dummyContractAddress is not found")
+    }
 
     const ethers = hre.ethers;
 
@@ -45,12 +55,17 @@ task(
     const env =
       process.env.ENV === "mainnet" ? Environment.MAINNET : Environment.TESTNET;
     const queryApi = new AxelarQueryAPI({ environment: env });
-    queryApi.estimateGasFee(hre.network.name, destinationChain, )
+    const chains = env === Environment.MAINNET ? mainnetChains : testnetChains;
+    const chain = chains[hre.network.name];
+    const gasFee = await queryApi.estimateGasFee(chain.id, destinationChain, chain.tokenSymbol);
 
     const tx = await sender.sendProposal(
       destinationChain,
       executorContractAddress,
-      calls
+      calls,
+      {
+        value: gasFee.toString(),
+      }
     );
 
     console.log("Sent transaction hash:", tx.hash);
