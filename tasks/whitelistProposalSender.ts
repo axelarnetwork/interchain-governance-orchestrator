1d0f46a14dc5dd93f480d6c784f8716e9c307907
npty <nptytn2@gmail.com>
2023-07-25 15:07:25 +0700

    chore: fix tasks

--- tasks/whitelistProposalSender.ts
+++ tasks/whitelistProposalSender.ts
@@ -1,29 +1,41 @@
-import { task } from "hardhat/config";
-import { InterchainProposalExecutor } from "../typechain-types/contracts/InterchainProposalExecutor";
-import { getInterchainProposalSenderAddress } from "./helpers/deployment";
+import { task } from 'hardhat/config';
+import {
+  getDeploymentAddress,
+  getInterchainProposalSenderAddress,
+} from './helpers/deployment';
 
-task("whitelistSender", "Whitelist proposal sender")
-  .addPositionalParam("sourceChain")
-  .addOptionalParam("sourceSender")
+task('whitelistSender', 'Whitelist proposal sender')
+  .addPositionalParam('sourceChain')
+  .addOptionalParam('sourceSender')
   .setAction(async (taskArgs, hre) => {
     const { sourceChain, sourceSender } = taskArgs;
     const ethers = hre.ethers;
 
-    const interchainProposalSenderAddress = sourceSender || getInterchainProposalSenderAddress(sourceChain.toLowerCase())
+    const interchainProposalSenderAddress =
+      sourceSender ||
+      getInterchainProposalSenderAddress(sourceChain.toLowerCase());
 
     if (!interchainProposalSenderAddress) {
-      throw new Error(`InterchainProposalSender is not found on ${sourceChain}`)
+      throw new Error(
+        `InterchainProposalSender is not found on ${sourceChain}`,
+      );
     }
 
-    // get executor contract
-    const executor = await ethers.getContract<InterchainProposalExecutor>(
-      "InterchainProposalExecutor"
+    const executorAddress = getDeploymentAddress(
+      hre,
+      'InterchainProposalExecutor',
+      hre.network.name,
+    );
+
+    const executor = await ethers.getContractAt(
+      'InterchainProposalExecutor',
+      executorAddress,
     );
 
     const tx = await executor.setWhitelistedProposalSender(
       sourceChain,
       interchainProposalSenderAddress,
-      true
+      true,
     );
 
     console.log(`setWhitelistedProposalSender: ${tx.hash}`);
