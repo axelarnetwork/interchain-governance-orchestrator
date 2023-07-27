04f21177556bd16bf4e5266b70adcc2a197251ca
npty <nptytn2@gmail.com>
2023-07-27 14:40:25 +0700

    chore: added comment

--- contracts/deploy/Deployer.sol
+++ contracts/deploy/Deployer.sol
@@ -3,3 +3,5 @@
 pragma solidity ^0.8.0;
 
 import { Create3Deployer } from '@axelar-network/axelar-gmp-sdk-solidity/contracts/deploy/Create3Deployer.sol';
+
+// Note: import the `Create3Deployer` here so that the hardhat scripts can access it, especially for the `00-deploy-deployer.sol` script
